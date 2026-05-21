073aad0438a9742b4ccc31834b1de4c73c4ace4c
Grzegorz Wojewódzki <s22478@pjwstk.edu.pl>
2026-05-21 21:34:40 +0200

    Register PC service

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using APBD_PC_Components_API.Data;
+using APBD_PC_Components_API.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Add services to the container.
+
+builder.Services.AddScoped<IPCService, PCService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
