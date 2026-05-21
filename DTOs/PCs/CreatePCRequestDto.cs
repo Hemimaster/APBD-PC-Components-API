f0c1aa545b57f34256bd8bbd2a5f8a7494ad2cd9
Grzegorz Wojewódzki <s22478@pjwstk.edu.pl>
2026-05-22 00:18:48 +0200

    Add request DTO validation

--- DTOs/PCs/CreatePCRequestDto.cs
+++ DTOs/PCs/CreatePCRequestDto.cs
@@ -1,14 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace APBD_PC_Components_API.DTOs.PCs;
 
 public class CreatePCRequestDto
 {
+    [Required]
+    [MaxLength(50)]
     public string Name { get; set; } = null!;
 
+    [Range(0.01, double.MaxValue)]
     public double Weight { get; set; }
 
+    [Range(0, int.MaxValue)]
     public int Warranty { get; set; }
 
+    [Required]
     public DateTime CreatedAt { get; set; }
 
+    [Range(0, int.MaxValue)]
     public int Stock { get; set; }
 }
